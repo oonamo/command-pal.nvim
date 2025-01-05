37ca80f3363aea9e830fc3b0760b1be0fb74186f
oonamo <onam75hernandez@gmail.com>
2025-01-04 19:09:21 -0800

    feat(mini_pick): Add highlighted keymaps
    This commit adds highlighted keymaps to the mini_pick palette

--- lua/command_pal/mini_pick.lua
+++ lua/command_pal/mini_pick.lua
@@ -11,6 +11,15 @@ H.opts = {
     { width = 0.7 },
     { width = 10, remaining = true },
   },
+  order = {
+    'text',
+    'keymap',
+  },
+}
+
+H.ids = {
+  keymap = vim.api.nvim_create_namespace('command_pal_keymap'),
+  desc = vim.api.nvim_create_namespace('command_pal_desc'),
 }
 
 local _, minipick = pcall(require, 'mini.pick')
@@ -71,12 +80,13 @@ local function entry_display(opts)
   H.compiled_width = H.calculate_widths(opts)
   return function(entry)
     local comp_str = ''
-    -- for i, v in ipairs(H.compiled_width) do
-    --   if entry[i] then comp_str = comp_str .. pad_str(entry[i], v) .. (opts.separator or ' ') end
-    -- end
+    for i, v in ipairs(H.compiled_width) do
+      if entry[i] then comp_str = comp_str .. pad_str(entry[i], v) .. (opts.separator or ' ') end
+    end
     return {
-      text = entry.command,
+      text = entry.command or entry.name or entry.command or entry.cmd_str,
       -- text = comp_str,
+      keymap = entry.keymap,
       name = entry.name,
       handler = entry.handler,
       command = entry.command,
@@ -106,6 +116,7 @@ local function format_item(item)
     item.desc,
     -- item.cmd_str,
     name = item.name,
+    keymap = item.keymap,
     desc = item.desc,
     handler = item.handler,
     command = item.command,
@@ -119,7 +130,7 @@ local function get_items(results)
   for k, v in pairs(results) do
     items[k] = format_item(v)
   end
-  vim.print(items)
+  -- vim.print(items)
   return items
 end
 
@@ -140,6 +151,35 @@ local function get_minipick_win_opts()
   return vim.tbl_deep_extend('force', M.opts.mini_pick.opts, ivy)
 end
 
+local function show_keymap(buf_id, items, query, pos, line, i)
+  if not items[line] then return end
+  if items[line].keymap and items[line].keymap ~= '' and M.opts.mini_pick.show_key then
+    local width = H.compiled_width[i] - #items[line].keymap
+    local text = { { pad_str('(' .. items[line].keymap .. ')', width), 'Constant' } }
+    vim.api.nvim_buf_set_extmark(buf_id, H.ids.keymap, line - 1, 0, {
+      virt_text = text,
+      virt_text_win_col = pos,
+      hl_mode = 'combine',
+    })
+  end
+end
+
+local function show(buf_id, items, query)
+  vim.api.nvim_buf_clear_namespace(buf_id, H.ids.keymap, 0, -1)
+  require('mini.pick').default_show(buf_id, items, query, { show_icons = false })
+  for line, item in ipairs(items) do
+    local width = 0
+    if item.text and type(item.text) == 'string' then
+      local text_width = #item.text
+      show_keymap(buf_id, items, query, text_width + 2, line, 1)
+    end
+    -- for i = 1, #H.opts.items do
+    --   width = width + (M._cache.compiled_width[i - 1] or 0)
+    --   if H[H.opts.order[i]] then H[H.opts.order[i]](buf_id, items, query, width, line, i) end
+    -- end
+  end
+end
+
 ---Picks using MiniNvim
 ---@param opts CommandPalConfig
 ---@param results palette.MappedAction
@@ -151,6 +191,7 @@ function M.pick(opts, results)
     source = {
       name = opts.mini_pick.title,
       items = get_items(results),
+      show = show,
       choose = function(item)
         -- FIXME: might be better to use pick's api for making sure the window is closed
         vim.schedule(function() item:handler() end)
