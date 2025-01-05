17e66052dee5c4764bd4c7ede851a3e98a675765
oonamo <onam75hernandez@gmail.com>
2025-01-04 22:38:47 -0800

    fix(handler): Silence error and fallback for default handler
    The default handler now checks if vim.cmd was successful under a
    protected call.
    If the command failed, fallback to setting it as the command line

--- lua/command_pal/handler.lua
+++ lua/command_pal/handler.lua
@@ -2,7 +2,8 @@ local M = {}
 
 function M.default_handler(v)
   if type(v.command) == 'string' then
-    vim.cmd(v.command)
+    local ok, err = pcall(vim.cmd, v.command)
+    if not ok then local set_cmd = require('command_pal.utils').set_cmdline(v.command .. " ")() end
     return 'vim cmd'
   elseif type(v.command) == 'function' then
     v.command()
